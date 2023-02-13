c090cc454b467ab856332bc3500be10b452220fd
Tiago Rodrigues <tiagoasrodrigues@protonmail.com~>
2023-02-13 19:52:54 -0300

    :sparkles: Add component Task

---
+++ src/components/Task/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1
+  },
+
+  taskName: {
+    color: "#f2f2f2",
+  }
+});
