d31336c59254f495764f29143f9478c52e3685f5
Tiago Rodrigues <tiagoasrodrigues@protonmail.com~>
2023-02-17 21:33:18 -0300

    :wrench: Setup component Task

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,13 +1,25 @@
-import { Text, View } from "react-native";
+import { Button, Image, Text, TouchableOpacity, View } from "react-native";
 
 import { styles } from './styles';
 
 export function Task() {
   return (
     <View style={styles.container}>
+      <TouchableOpacity style={styles.button}>
+        <Image
+          source={require('../../../assets/trash.png')}
+        />
+      </TouchableOpacity>
+
       <Text style={styles.taskName}>
-        Hello World!
+        Integer urna interdum massa libero auctor neque turpis turpis semper.
       </Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Image
+          source={require('../../../assets/trash.png')}
+        />
+      </TouchableOpacity>
     </View>
   )
 }
--- src/components/Task/styles.ts
+++ src/components/Task/styles.ts
@@ -2,12 +2,24 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#262626',
+    width: '100%',
+    height: 64,
+    marginBottom: 8,
+    borderRadius: 4,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1
+    justifyContent: 'space-between',
   },
 
   taskName: {
     color: "#f2f2f2",
+    flex: 1,
+    fontSize: 14,
+    textAlign: 'justify',
+  },
+
+  button: {
+    margin: 10
   }
 });
