2e1ba76f29ae6eab508e3ef615f0245e71961d3e
Tiago Rodrigues <tiagoasrodrigues@protonmail.com~>
2023-02-20 13:25:47 -0300

    :sparkles: Component Task

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,25 +1,27 @@
-import { Button, Image, Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import { Checkbox } from "../Checkbox";
+import { DeleteButton } from "../DeleteButton";
 
 import { styles } from './styles';
 
 export function Task() {
+  let task = 'Integer urna interdum massa libero auctor neque turpis turpis semper.'
+
+  function handleTask() {
+    console.log('Conclui uma task 💕');
+  }
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.button}>
-        <Image
-          source={require('../../../assets/trash.png')}
-        />
+      <TouchableOpacity style={styles.taskCheckbox} onPress={handleTask}>
+        <Checkbox />
+
+        <Text style={styles.taskName}>
+          {task}
+        </Text>
       </TouchableOpacity>
 
-      <Text style={styles.taskName}>
-        Integer urna interdum massa libero auctor neque turpis turpis semper.
-      </Text>
-
-      <TouchableOpacity style={styles.button}>
-        <Image
-          source={require('../../../assets/trash.png')}
-        />
-      </TouchableOpacity>
+      <DeleteButton />
     </View>
   )
 }
--- src/components/Task/styles.ts
+++ src/components/Task/styles.ts
@@ -3,23 +3,31 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     backgroundColor: '#262626',
-    width: '100%',
     height: 64,
+    marginLeft: 24,
+    marginRight: 24,
     marginBottom: 8,
     borderRadius: 4,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between',
+  },
+
+  taskCheckbox: {
+    flexDirection: 'row',
   },
 
   taskName: {
     color: "#f2f2f2",
     flex: 1,
     fontSize: 14,
-    textAlign: 'justify',
+    textAlignVertical: "center",
   },
 
-  button: {
-    margin: 10
+  taskConcluded: {
+    color: "#808080",
+    flex: 1,
+    fontSize: 14,
+    textAlignVertical: "center",
+    textDecorationLine: "line-through"
   }
 });
